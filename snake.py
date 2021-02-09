18105ce7a33555ea6e0a7ed17b504b167cb491ff
Искандер <iskin20003@gmail.com>
2021-02-09 17:29:26 +0300

    Restart and apple sprite

--- snake.py
+++ snake.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 import sys
 import random
 import time
@@ -12,7 +13,29 @@ BROWN = pygame.Color(165, 42, 42)
 FPS_CONTROL = pygame.time.Clock()
 
 
-class Game():
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+
+all_sprites = pygame.sprite.Group()
+
+# создадим спрайт
+sprite = pygame.sprite.Sprite()
+# определим его вид
+sprite.image = load_image("apple.png")
+# и размеры
+sprite.rect = sprite.image.get_rect()
+
+all_sprites.add(sprite)
+
+
+class Game:
     def __init__(self):
 
         self.width = 800
@@ -39,13 +62,14 @@ class Game():
                     change = "UP"
                 elif event.key == pygame.K_DOWN or event.key == ord("s"):
                     change = "DOWN"
+                elif event.key == ord("r"):
+                    pass
 
                 elif event.key == pygame.K_ESCAPE:
                     pygame.quit()
                     sys.exit()
         return change
 
-    # переделать
     def refresh(self):
         pygame.display.flip()
         FPS_CONTROL.tick(20)
@@ -66,15 +90,22 @@ class Game():
         go_surf = go_font.render('Game over', True, RED)
         go_rect = go_surf.get_rect()
         go_rect.midtop = (360, 15)
+        back_font = pygame.font.SysFont('comicsansms', 40)
+        restart = back_font.render("Press R to Restart", True, GREEN)
+        restart_rect = restart.get_rect()
+        restart_rect.midtop = (360, 165)
+        escape = back_font.render("Press E to Escape", True, GREEN)
+        escape_rect = escape.get_rect()
+        escape_rect.midtop = (360, 225)
         self.surface.blit(go_surf, go_rect)
+        self.surface.blit(restart, restart_rect)
+        self.surface.blit(escape, escape_rect)
         self.show_score(0)
         pygame.display.flip()
-        time.sleep(3)
-        pygame.quit()
-        sys.exit()
+        time.sleep(1)
 
 
-class Snake():
+class Snake:
     def __init__(self, snake_color):
         self.head = [100, 100]
         self.body = [[100, 100], [90, 100], [80, 100]]
@@ -124,48 +155,71 @@ class Snake():
                 self.head[1] > height - 10
                 or self.head[1] < 0
         )):
-            game_over()
+            while True:
+                game_over()
+                for event in pygame.event.get():
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == ord("r"):
+                            play()
+                        elif event.key == ord("e"):
+                            pygame.quit()
+                            sys.exit()
         for block in self.body[1:]:
             if block[0] == self.head[0] and block[1] == self.head[1]:
-                game_over()
+                while True:
+                    game_over()
+                    for event in pygame.event.get():
+                        if event.type == pygame.KEYDOWN:
+                            if event.key == ord("r"):
+                                play()
+                            elif event.key == ord("e"):
+                                pygame.quit()
+                                sys.exit()
 
 
-class Food():
+class Food:
     def __init__(self, food_color, width, height):
         """Инит еды"""
         self.food_color = food_color
         self.food_size_x = 10
         self.food_size_y = 10
-        self.food_pos = [random.randrange(1, width/10)*10,
-                         random.randrange(1, height/10)*10]
+        self.food_pos = [random.randrange(1, width / 10) * 10,
+                         random.randrange(1, height / 10) * 10]
 
     def draw_food(self, surface):
         """Отображение еды"""
+        sprite.rect.x = self.food_pos[0]
+        sprite.rect.y = self.food_pos[1]
         pygame.draw.rect(
-            surface, self.food_color, pygame.Rect(
+            surface, (255, 255, 255), pygame.Rect(
                 self.food_pos[0], self.food_pos[1],
                 self.food_size_x, self.food_size_y))
+        all_sprites.draw(surface)
 
 
-game = Game()
-snake = Snake(GREEN)
-food = Food(BROWN, game.width, game.height)
+def play():
+    game = Game()
+    snake = Snake(GREEN)
+    food = Food(BROWN, game.width, game.height)
 
-game.init()
+    game.init()
 
-while True:
-    snake.change = game.event_loop(snake.change)
+    while True:
+        snake.change = game.event_loop(snake.change)
 
-    snake.validate_change()
-    snake.change_head()
-    game.score, food.food_pos = snake.snake_body(
-        game.score, food.food_pos, game.width, game.height)
-    snake.draw(game.surface, WHITE)
+        snake.validate_change()
+        snake.change_head()
+        game.score, food.food_pos = snake.snake_body(
+            game.score, food.food_pos, game.width, game.height)
+        snake.draw(game.surface, WHITE)
 
-    food.draw_food(game.surface)
+        food.draw_food(game.surface)
 
-    snake.check_collisions(
-        game.game_over, game.width, game.height)
+        snake.check_collisions(
+            game.game_over, game.width, game.height)
 
-    game.show_score()
-    game.refresh()
+        game.show_score()
+        game.refresh()
+
+
+play()
